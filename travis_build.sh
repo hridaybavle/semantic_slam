ea242b01e9d70bfd884fe9178274e0f52c126bb7
hriday <hriday.bavle@gmail.com>
2020-07-03 18:31:24 +0200

    final travis build step

---
+++ travis_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p /root/semantic_slam_build_ws/src
+cd /root/semantic_slam_build_ws/src
+git clone https://github.com/hridaybavle/semantic_slam.git 
+git clone https://bitbucket.org/hridaybavle/bucket_detector.git 
+source /opt/ros/kinetic/setup.bash
+cd .. && catkin build --cmake-args -DCMAKE_BUILD_TYPE=Release
